be604fa78af8f1015781a32dc59fcd28d689eb7e
Jesus Vergara <jesusmanuelv1989@gmail.com>
2024-08-06 20:33:20 -0600

    updating the quantities in the cart truly chatGPT help me to finf the bug

--- myshop/cart/cart.py
+++ myshop/cart/cart.py
@@ -18,7 +18,7 @@ class Cart:
              self.cart[product_id]['quantity'] = quantity
         else:
             self.cart[product_id]['quantity'] += quantity
-            self.save()
+        self.save()
 
     def save(self):
         self.session.modified = True
