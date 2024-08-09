fcffdc54ea137170f0932d5943ec29dd80111751
Jesus Vergara <jesusmanuelv1989@gmail.com>
2024-08-08 18:45:29 -0600

    adding created, updated and order detail to the order

--- myshop/orders/models.py
+++ myshop/orders/models.py
@@ -12,7 +12,8 @@ class Order(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     paid = models.BooleanField(default=False)
-
+    stripe_id = models.CharField(max_length=250, blank=True)
+    
     class Meta:
         ordering = ['-created']
         indexes = [
@@ -25,7 +26,6 @@ class Order(models.Model):
     def get_total_cost(self):
         return sum(item.get_cost() for item in self.items.all())
     
-    stripe_id = models.CharField(max_length=250, blank=True)
 
     def get_stripe_url(self):
            if not self.stripe_id:
