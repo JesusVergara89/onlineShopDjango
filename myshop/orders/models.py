862a1f2acbfe68692fbee154e3810b470058597a
Jesus Vergara <jesusmanuelv1989@gmail.com>
2024-08-10 20:51:47 -0600

    adding coupones

--- myshop/orders/models.py
+++ myshop/orders/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 from shop.models import Product
 from django.conf import settings
+from decimal import Decimal
+from django.core.validators import MinValueValidator, \
+                                   MaxValueValidator
+from coupons.models import Coupon 
 
 class Order(models.Model):
     first_name = models.CharField(max_length=50)
@@ -24,8 +28,8 @@ class Order(models.Model):
         return f'Order {self.id}'
     
     def get_total_cost(self):
-        return sum(item.get_cost() for item in self.items.all())
-    
+        total_cost = self.get_total_cost_before_discount()
+        return total_cost - self.get_discount()    
 
     def get_stripe_url(self):
            if not self.stripe_id:
@@ -38,6 +42,18 @@ class Order(models.Model):
                # Stripe path for real payments
                path = '/'
            return f'https://dashboard.stripe.com{path}payments/{self.stripe_id}'
+    
+    coupon = models.ForeignKey(Coupon,related_name='orders',null=True,blank=True,on_delete=models.SET_NULL)
+    discount = models.IntegerField(default=0,validators=[MinValueValidator(0),MaxValueValidator(100)])
+
+    def get_total_cost_before_discount(self):
+        return sum(item.get_cost() for item in self.items.all())
+    
+    def get_discount(self):
+        total_cost = self.get_total_cost_before_discount()
+        if self.discount:
+            return total_cost * (self.discount / Decimal(100))
+        return Decimal(0)
 
 class OrderItem(models.Model):
     order = models.ForeignKey(Order,related_name='items',on_delete=models.CASCADE)
